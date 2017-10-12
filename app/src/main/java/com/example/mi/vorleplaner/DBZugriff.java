9192008471cf3340e9e4769f170d65960c77f03a
Robadd <robaddk@web.de>
2017-10-12 20:56:02 +0200

    fixi

--- app/src/main/java/com/example/mi/vorleplaner/DBZugriff.java
+++ app/src/main/java/com/example/mi/vorleplaner/DBZugriff.java
@@ -11,6 +11,19 @@ import android.util.Log;
 
 public class DBZugriff extends SQLiteOpenHelper {
 
+    public static final String DB_NAME = "vorleplaner.db";
+
+    public static final String TABLE_PROFESSOREN = "Professoren";
+    public static final String TABLE_VORLESUNGEN = "Vorlesungen";
+    public static final String TABLE_VORLESUNGEN_PROFESSOREN = "Vorlesungs_Professor";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_PROFESSOREN_NAME = "Name";
+    public static final String COLUMN_VORLESUNGEN_NAME = "Name";
+    public static final String COLUMN_VORLESUNGEN_WEEKDAY = "Wochentag";
+    public static final String COLUMN_VORLESUNGEN_STARTTIME = "Startzeit";
+    public static final String COLUMN_VORLESUNGEN_ENDTIME = "Endzeit";
+
     private SQLiteDatabase db;
     public static final String TAG = "DBZugriff";
 
@@ -21,33 +34,29 @@ public class DBZugriff extends SQLiteOpenHelper {
 
     public void onCreate(SQLiteDatabase db){
         try{
-            String tableVorlesungen = "CREATE TABLE Vorlesungen (id INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                    "name VARCHAR(50) NOT NULL, "+
-                    "vorlesungsZeit_id INTEGER NOT NULL)";
-            String tableProfessoren = "CREATE TABLE Professoren (id INTEGER PRIMARY KEY AUTOINCREMENT,"+
-                    "name VARCHAR(50) NOT NULL)";
-            String tableZeiten = "CREATE TABLE Zeiten (id INTEGER PRIMARY KEY AUTOINCREMEN),"+
-                    "wochentag VARCHAR(10) NOT NULL,"+
-                    "uhrzeit VARCHAR(10) NOT NULL)";
-            String tableVorlProf = "CREATE TABLE Vorlesungs_Professor (id INTEGER PRIMARY KEY AUTOINCREMENT,"+
-                    "vorlesung_id INTEGER NOT NULL,"+
-                    "professor_id INTEGER NOT NULL)";
+            String tableVorlesungen = "CREATE TABLE " + TABLE_VORLESUNGEN + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    COLUMN_VORLESUNGEN_NAME + " VARCHAR(50) NOT NULL, "+
+                    COLUMN_VORLESUNGEN_WEEKDAY + " INTEGER NOT NULL, " +
+                    COLUMN_VORLESUNGEN_STARTTIME + " INTEGER NOT NULL, " +
+                    COLUMN_VORLESUNGEN_ENDTIME + " INTEGER NOT NULL)";
+
+            String tableProfessoren = "CREATE TABLE " + TABLE_PROFESSOREN + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    COLUMN_PROFESSOREN_NAME + " VARCHAR(50) NOT NULL)";
+
+            String tableVorlProf = "CREATE TABLE " + TABLE_VORLESUNGEN_PROFESSOREN + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    TABLE_PROFESSOREN + "_id INTEGER NOT NULL, "+
+                    TABLE_VORLESUNGEN + "_id INTEGER NOT NULL)";
 
             db.execSQL(tableProfessoren);
             db.execSQL(tableVorlesungen);
-            db.execSQL(tableZeiten);
             db.execSQL(tableVorlProf);
-
-
+            Log.d(TAG, "DB created");
         } catch(Exception e){
             Log.e(TAG, e.getMessage());
         }
     }
 
-
-    public void onUpgrade(SQLiteDatabase db, int a, int b){
-
-    }
+    public void onUpgrade(SQLiteDatabase db, int a, int b){}
 
     public synchronized void close(){
         if(db != null){
@@ -56,7 +65,4 @@ public class DBZugriff extends SQLiteOpenHelper {
         }
         super.close();
     }
-
-
-
 }
--- app/src/main/java/com/example/mi/vorleplaner/ProfessorenActivity.java
+++ app/src/main/java/com/example/mi/vorleplaner/ProfessorenActivity.java
@@ -3,46 +3,57 @@ package com.example.mi.vorleplaner;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 
 public class ProfessorenActivity extends AppCompatActivity {
 
     private static final String TAG = "PROFESSOREN_ACTIVITY";
-    private DBZugriff db;
+    private DBZugriff dbZugriff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professoren);
-        db = new DBZugriff(this,"VorLePlaner");
+        dbZugriff = new DBZugriff(this,"VorLePlaner");
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                LayoutInflater inflater = getLayoutInflater();
+                View alertLayout = inflater.inflate(R.layout.input_professoren, null);
+                final EditText inputName = (EditText) alertLayout.findViewById(R.id.textInputProfessoren);
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
                 builder.setTitle("Hinzufügen");
-                builder.setView(R.layout.input_professoren);
+                builder.setView(alertLayout);
 
                 builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int which){
-                        Log.d(TAG,"ok");
                         ContentValues daten = new ContentValues();
-                        Log.d( TAG, "name: "+ ((EditText)findViewById(R.id.textInputProfessoren)).getText().toString() );
+                        String name = inputName.getText().toString();
+                        daten.put(DBZugriff.COLUMN_PROFESSOREN_NAME, name);
+                        SQLiteDatabase db = dbZugriff.getWritableDatabase();
+                        long insertId = db.insert(DBZugriff.TABLE_PROFESSOREN, null, daten);
+                        Log.d(TAG, String.valueOf(insertId));
                     }
                 });
+
                 builder.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int which){
-                        Log.d(TAG,"Abbrechen");
+
                     }
                 });
+
                 builder.show();
             }
         });
